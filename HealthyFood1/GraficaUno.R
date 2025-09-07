869cf6e3ed5c4e270ccd26ab1df1721a597c3d6d
andresr.cacereso@konradlorenz.edu.co <andresr.cacereso@konradlorenz.edu.co>
2025-09-07 16:52:48 -0500

    Primera grafica agregada

---
+++ HealthyFood1/GraficaUno.R
@@ -0,0 +1,26 @@
+library(readxl)
+library(tidyverse)
+library(moments)
+
+# Leer archivo Excel
+df <- read_excel("C:/Users/andre/Downloads/Opiniones_corregido_numeros_puros.xlsx")
+
+# Revisar nombres de columna (porque R renombra columnas duplicadas)
+names(df)
+
+# Conversión de columnas a numéricas (ajustado para nombre con puntos)
+df <- df %>%
+  mutate(
+    Edad = as.numeric(Edad),
+    Frecuencia_Consumo = as.numeric(Frecuencia_Consumo),
+    Horas_Contenido_Nutricion = as.numeric(Horas_Contenido_Nutricion),
+    Disposicion_Pago_Num = as.numeric(Disposicion_Pago_Num...10)  # Usa esta columna
+  )
+
+ggplot(df, aes(x = factor(Disposicion_Pago_Num), fill = Sexo)) +
+  geom_bar(position = "dodge") +
+  labs(title = "Disposición a Pagar (%) por Sexo",
+       x = "Porcentaje adicional",
+       y = "Frecuencia",
+       fill = "Sexo") +
+  theme_minimal()
